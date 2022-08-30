b1461813cc8e37c8210366072f2f6f0af391d3a6
Wang Pei <E0376916@u.nus.edu>
2022-08-30 14:40:56 +0800

    Add retrieve APIs in DataRetriever

--- Team34/Code34/SPA/QPS/DataRetriever.h
+++ Team34/Code34/SPA/QPS/DataRetriever.h
@@ -1,11 +1,20 @@
 #pragma once
+
+#include "relation/UsesSRel.h"
+#include "relation/UsesPRel.h"
+#include "relation/ModifiesSRel.h"
 #include "relation/ModifiesPRel.h"
+#include "QueryResult.h"
 
 class DataRetriever
 {
 public:
-	bool retrieve(const ModifiesPRel rel) {
+	const QueryResult& retrieve(const ModifiesPRel rel);
 
-	}
+	const QueryResult& retrieve(const UsesPRel rel);
+
+	const QueryResult& retrieve(const ModifiesSRel rel);
+
+	const QueryResult& retrieve(const ModifiesPRel rel);
 };
 
