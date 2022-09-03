3d3f89a3e0b3b1a9f8813a6fb948f66b42b92610
Gao Gui <e0376992@u.nus.edu>
2022-09-03 23:11:40 +0800

    edit spa config

--- Team34/Code34/AutoTester/source/TestWrapper.cpp
+++ Team34/Code34/AutoTester/source/TestWrapper.cpp
@@ -1,4 +1,5 @@
 #include "TestWrapper.h"
+#include "../SPA/SP/SourceParser.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -19,6 +20,8 @@ TestWrapper::TestWrapper() {
 void TestWrapper::parse(std::string filename) {
 	// call your parser to do the parsing
   // ...rest of your code...
+	SourceParser parser = SourceParser();
+	ProgramNode node = parser.parse(filename);
 }
 
 // method to evaluating a query
