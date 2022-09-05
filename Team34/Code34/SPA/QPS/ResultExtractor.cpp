19c3b3af2262d0c6c9d2a338529825555ee6b152
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2022-09-05 08:12:32 +0800

    Revert "Update ResultExtractor.cpp"
    This reverts commit 40683c36f02a9f42fa4c9b48bf4d0d2006e204ad.

--- Team34/Code34/SPA/QPS/ResultExtractor.cpp
+++ Team34/Code34/SPA/QPS/ResultExtractor.cpp
@@ -3,7 +3,6 @@
 #include <assert.h>
 #include <algorithm>
 #include "../Utils/type/RefType.h"
-#include <iostream>
 
 string ResultExtractor::GetFormattedResult() {
 
@@ -40,12 +39,8 @@ string ResultExtractor::GetSingleSynResult() {
 		result_set = CombineResult(result_set, domain_from_table);
 		
 	}
+	return FormatResult(result_set, ref->GetRefType());
 
-	string formatted_result = FormatResult(result_set, ref->GetRefType());
-
-	std::cout << "test" << std::endl;
-
-	return formatted_result;
 }
 
 string ResultExtractor::GetMultiSynResult() {
