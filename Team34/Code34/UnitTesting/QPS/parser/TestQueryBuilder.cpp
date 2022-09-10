aa302d37d9dc2238363b25c508530b5b1dcb99a0
Nauman Sajid <e0389102@u.nus.edu>
2022-09-11 00:09:20 +0800

    Test Case for such that clause

--- Team34/Code34/UnitTesting/QPS/parser/TestQueryBuilder.cpp
+++ Team34/Code34/UnitTesting/QPS/parser/TestQueryBuilder.cpp
@@ -18,10 +18,9 @@ namespace UnitTesting
 
 		TEST_METHOD(TestValidSelectAllVariableQuery)
 		{
-			const std::string valid_select_all_query_ = "variable V; Select V";
+			const std::string query_ = "variable V; Select V";
 			const std::string var = "V";
-
-			shared_ptr<Query> query = query_builder_->GetQuery(valid_select_all_query_);
+			shared_ptr<Query> query = query_builder_->GetQuery(query_);
 
 			//Check if entity references are correct
 			Assert::IsTrue(query->GetSelectTuple()->size() == 1);
@@ -35,6 +34,35 @@ namespace UnitTesting
 			//Check if all pattern clauses are correct
 			Assert::IsTrue(query->GetPatterns()->size() == 0);
 		}
+
+		TEST_METHOD(TestValidBasicSuchThat) {
+
+			const std::string query_ = "variable V; Select V such that Uses(3,V)";
+			const std::string var_ = "V";
+			std::string lhs_value_ = "3";
+			std::string rhs_value_ = var_;
+
+			shared_ptr<Query> query = query_builder_->GetQuery(query_);
+
+			//Check if entity references are correct
+			Assert::IsTrue(query->GetSelectTuple()->size() == 1);
+			Assert::IsTrue(query->GetSelectTuple()->at(0)->GetRefType() == RefType::kVarRef);
+			Assert::IsTrue(query->GetSelectTuple()->at(0)->GetValType() == ValType::kSynonym);
+			Assert::AreEqual(query->GetSelectTuple()->at(0)->GetName(), var_);
+
+			//Check if such that clauses are correct
+			Assert::IsTrue(query->GetRelations()->size() == 1);
+			Assert::IsTrue(query->GetRelations()->at(0)->GetRelType() == RelType::kUsesSRel);
+			Assert::IsTrue(query->GetRelations()->at(0)->LhsRefType() == RefType::kStmtRef);
+			Assert::AreEqual(query->GetRelations()->at(0)->LhsValue() , lhs_value_);
+			Assert::IsTrue(query->GetRelations()->at(0)->ValTypes().first == ValType::kConst);
+			Assert::IsTrue(query->GetRelations()->at(0)->RhsRefType() == RefType::kVarRef);
+			Assert::AreEqual(query->GetRelations()->at(0)->RhsValue(), rhs_value_);
+			Assert::IsTrue(query->GetRelations()->at(0)->ValTypes().second == ValType::kSynonym);
+
+			//Check if all pattern clauses are correct
+			Assert::IsTrue(query->GetPatterns()->size() == 0);
+		};
 
 	};
 }
