d742485dc36bdd3a22a653760c3deed1045e242f
tanzhuoyao-nus <tanzhuoyao@gmail.com>
2022-09-15 21:55:15 +0800

    Refactor PatternManager to ExprSpec

--- Team34/Code34/SPA/PKB/manager/PatternManager.cpp
+++ Team34/Code34/SPA/PKB/manager/PatternManager.cpp
@@ -6,26 +6,19 @@ void PatternManager::AddAssignPattern(StmtNum stmt_num, Variable var, std::strin
 	assign_statement_map_[stmt_num] = std::make_pair(var, expr);
 }
 
-bool PatternManager::IsAssignPatternMatch(StmtNum stmt_num, std::string expr, bool is_exact_match)
+bool PatternManager::IsAssignPatternMatch(StmtNum stmt_num, std::shared_ptr<ExprSpec> expr)
 {
 	auto iter = assign_statement_map_.find(stmt_num);
 	if (iter != assign_statement_map_.end())
 	{
 		std::pair<Variable, std::string> pair = assign_statement_map_[stmt_num];
 		std::string stored_expr = pair.second;
-		if (is_exact_match && expr == stored_expr)
-		{
-			return true;
-		}
-		if (!is_exact_match && stored_expr.find(expr) != std::string::npos)
-		{
-			return true;
-		}
+		return expr->IsMatch(Expr(stored_expr));
 	}
 	return false;
 }
 
-bool PatternManager::IsAssignPatternMatch(StmtNum stmt_num, Variable var, std::string expr, bool is_exact_match)
+bool PatternManager::IsAssignPatternMatch(StmtNum stmt_num, Variable var, std::shared_ptr<ExprSpec> expr)
 {
 	auto iter = assign_statement_map_.find(stmt_num);
 	if (iter != assign_statement_map_.end())
@@ -33,30 +26,22 @@ bool PatternManager::IsAssignPatternMatch(StmtNum stmt_num, Variable var, std::s
 		std::pair<Variable, std::string> pair = assign_statement_map_[stmt_num];
 		Variable stored_var = pair.first;
 		std::string stored_expr = pair.second;
-		if (is_exact_match && var == stored_var && expr == stored_expr)
+		if (var == stored_var)
 		{
-			return true;
-		}
-		if (!is_exact_match && var == stored_var && stored_expr.find(expr) != std::string::npos)
-		{
-			return true;
+			return expr->IsMatch(Expr(stored_expr));
 		}
 	}
 	return false;
 }
 
-std::shared_ptr<std::vector<std::pair<StmtNum, Variable>>> PatternManager::GetAssignPatternMatch(std::string expr, bool is_exact_match)
+std::shared_ptr<std::vector<std::pair<StmtNum, Variable>>> PatternManager::GetAssignPatternMatch(std::shared_ptr<ExprSpec> expr)
 {
 	std::shared_ptr<std::vector<std::pair<StmtNum, Variable>>> all_sv_pairs = std::make_shared<std::vector<std::pair<StmtNum, Variable>>>();
 	for (auto iter = assign_statement_map_.begin(); iter != assign_statement_map_.end(); ++iter)
 	{
 		StmtNum stmt_num = iter->first;
 		std::pair<Variable, std::string> pair = iter->second;
-		if (is_exact_match && expr == pair.second)
-		{
-			all_sv_pairs->push_back(std::make_pair(stmt_num, pair.first));
-		}
-		if (!is_exact_match && pair.second.find(expr) != std::string::npos)
+		if (expr->IsMatch(Expr(pair.second)))
 		{
 			all_sv_pairs->push_back(std::make_pair(stmt_num, pair.first));
 		}
--- Team34/Code34/SPA/PKB/manager/PatternManager.h
+++ Team34/Code34/SPA/PKB/manager/PatternManager.h
@@ -6,15 +6,16 @@
 #include <vector>
 
 #include "../../Utils/type/TypeDef.h"
+#include "../../Utils/expression/ExprSpec.h"
 
 class PatternManager
 {
 public:
 	// Assign Pattern methods
 	void AddAssignPattern(StmtNum stmt_num, Variable var, std::string expr);
-	bool IsAssignPatternMatch(StmtNum stmt_num, std::string expr, bool is_exact_match);
-	bool IsAssignPatternMatch(StmtNum stmt_num, Variable var, std::string expr, bool is_exact_match);
-	std::shared_ptr<std::vector<std::pair<StmtNum, Variable>>> GetAssignPatternMatch(std::string expr, bool is_exact_match);
+	bool IsAssignPatternMatch(StmtNum stmt_num, std::shared_ptr<ExprSpec> expr);
+	bool IsAssignPatternMatch(StmtNum stmt_num, Variable var, std::shared_ptr<ExprSpec> expr);
+	std::shared_ptr<std::vector<std::pair<StmtNum, Variable>>> GetAssignPatternMatch(std::shared_ptr<ExprSpec> expr);
 private:
 	// A map that maps statement number -> pair<Variable, Postfix Expression>
 	std::unordered_map<StmtNum, std::pair<Variable, std::string>> assign_statement_map_;
