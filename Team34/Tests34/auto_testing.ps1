115647cd7022d998aa61d0c8dc88154525e6e0d1
Nauman Sajid <e0389102@u.nus.edu>
2022-09-16 11:53:48 +0800

    Replace Bash Script with power shell script

---
+++ Team34/Tests34/auto_testing.ps1
@@ -0,0 +1,35 @@
+Write-Output "AutoTester Script has begun"
+$autotester_file = "..\Code34\Debug\AutoTester.exe"
+$condition = Test-Path -Path $autotester_file -PathType Leaf
+
+if ($condition) {
+    Write-Output "Auto Tester Path is found"
+} else {
+    Write-Output "Auto Tester Path is not found"
+    Exit
+}
+
+
+$directories = Get-ChildItem -Directory
+
+foreach($folder in $directories) {
+    $files = Get-ChildItem $folder -File
+    $source_file = $null
+    $query_file = $null
+        foreach ($file in $files) {
+            if ($file.FullName -like "*source.txt") {
+                $source_file = $file.FullName
+            } elseif ($file.FullName -like "*queries.txt") {
+                $query_file = $file.FullName
+            }
+        }
+    if (!$source_file) {
+        Write-Output "Source file not found in folder $($folder), skipping folder"
+    } elseif(!$query_file) {
+        Write-Output "Query file not found int folder $($folder), skipping folder"
+    } else {
+        $output_folder = "$($folder.FullName)\out.xml"
+        Write-Output $output_folder
+       & $autotester_file $source_file $query_file $output_folder
+    }
+}
