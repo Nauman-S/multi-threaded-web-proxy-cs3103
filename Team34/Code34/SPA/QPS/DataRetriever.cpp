f885f7b39cfb9e3f4820722aa6bfaf5ff4d6c98d
tanzhuoyao-nus <tanzhuoyao@gmail.com>
2022-09-18 18:11:29 +0800

    Fix DataRetriever bug

--- Team34/Code34/SPA/QPS/DataRetriever.cpp
+++ Team34/Code34/SPA/QPS/DataRetriever.cpp
@@ -540,7 +540,7 @@ shared_ptr<unordered_set<int>> DataRetriever::FilterStmtSetByType(shared_ptr<uno
 
 shared_ptr<vector<pair<int, string>>> DataRetriever::FilterStmtTableByType(shared_ptr<vector<pair<int, string>>> table, RefType stmt_type)
 {
-    shared_ptr<vector<pair<int, string>>> res;
+    auto res = make_shared<vector<pair<int, string>>>();
     auto type_predicate = [this, stmt_type] (int stmt) -> bool { return *(this->pkb_ptr_->GetStatementType(stmt)) == stmt_type; };
     for (auto iter=table->begin(); iter != table->end(); ++iter) {
         if (type_predicate(iter->first)) {
@@ -553,7 +553,7 @@ shared_ptr<vector<pair<int, string>>> DataRetriever::FilterStmtTableByType(share
 
 shared_ptr<vector<pair<int, int>>> DataRetriever::FilterStmtTableByType(shared_ptr<vector<pair<int, int>>> table, RefType lhs_stmt_type, RefType rhs_stmt_type)
 {
-    shared_ptr<vector<pair<int, int>>> res;
+    auto res = make_shared<vector<pair<int, int>>>();
     auto lhs_type_predicate = [this, lhs_stmt_type](int stmt) -> bool { return *(this->pkb_ptr_->GetStatementType(stmt)) == lhs_stmt_type; };
     auto rhs_type_predicate = [this, rhs_stmt_type](int stmt) -> bool { return *(this->pkb_ptr_->GetStatementType(stmt)) == rhs_stmt_type; };
 
