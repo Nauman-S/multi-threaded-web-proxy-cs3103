513b6fe5d08b9aa9973c093666856fa1ec06292e
awp <49505301+wangpeialex@users.noreply.github.com>
2022-09-25 10:36:38 +0800

    Refactor With clause

--- Team34/Code34/SPA/QPS/with_clause/With.h
+++ Team34/Code34/SPA/QPS/with_clause/With.h
@@ -6,6 +6,7 @@
 #include "../Clause.h"
 #include "../reference/Ref.h"
 #include "../../Utils/type/RefType.h" 
+#include "../reference/ValType.h"
 #include "../query_result/ResWrapper.h"
 class DataRetriever;
 
@@ -13,18 +14,24 @@ class With
 	:public Clause
 {
 protected:
-	std::shared_ptr<Ref> ref_ptr_;  // a synonym reference
-	std::string req_val_;  // required value
+	// either one of the reference shuold be synonym
+	std::shared_ptr<Ref> lhs_ref_ptr_;
+	std::shared_ptr<Ref> rhs_ref_ptr_;
+	bool is_valid_;
 
 public:
-	With(std::shared_ptr<Ref> ref, std::string val)
-		: ref_ptr_{ ref }, req_val_{ val } {};
+	explicit With(bool is_valid) : lhs_ref_ptr_{ nullptr }, rhs_ref_ptr_{ nullptr }, is_valid_{ is_valid } {};
 
-	RefType RefType() { return ref_ptr_->GetRefType(); }
+	With(std::shared_ptr<Ref> ref1, std::shared_ptr<Ref> ref2)
+		: lhs_ref_ptr_{ ref1 }, rhs_ref_ptr_{ ref2 }, is_valid_{ true } {};  // Expect LHS always be synonym reference
 
-	std::string RefName() { return ref_ptr_->GetName(); }
+	RefType RefType() { return lhs_ref_ptr_->GetRefType(); }  // LHS and RHS should be the same RefType
 
-	std::string RequiredValue() { return req_val_; }
+	std::pair<ValType, ValType> ValTypes() { return std::make_pair<ValType, ValType>(lhs_ref_ptr_->GetValType(), rhs_ref_ptr_->GetValType()); }
+
+	std::string LhsValue() { return lhs_ref_ptr_->GetName(); }
+
+	std::string RhsValue() { return rhs_ref_ptr_->GetName(); }
 
 	std::shared_ptr<ResWrapper> GetMatch(DataRetriever& retriever) override;
 
