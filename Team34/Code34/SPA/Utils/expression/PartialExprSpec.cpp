bdfe379bfabbbc1bd1c36aaed6ae7af99953c108
awp <49505301+wangpeialex@users.noreply.github.com>
2022-09-28 16:11:33 +0800

    Implement new substring matching

--- Team34/Code34/SPA/Utils/expression/PartialExprSpec.cpp
+++ Team34/Code34/SPA/Utils/expression/PartialExprSpec.cpp
@@ -2,5 +2,21 @@
 
 bool PartialExprSpec::IsMatch(Expr expr)
 {
-	return expr.GetPostfix().find(post_expr_) != string::npos;
+	string full_expr = expr.GetPostfix();
+	int fullstr_len = full_expr.size();
+	int substr_len = post_expr_.length();
+	auto is_sub_expr{ false };
+	int pos = full_expr.find(post_expr_, 0);
+
+	while (pos < fullstr_len) {
+		
+		if ((pos == 0 || full_expr[pos - 1] == ' ') && (pos + substr_len == fullstr_len || full_expr[pos + substr_len] == ' ')) {
+			is_sub_expr = true;
+			break;
+		}
+		pos = full_expr.find(post_expr_, pos + 1);
+	}
+
+	return is_sub_expr;
+
 }
