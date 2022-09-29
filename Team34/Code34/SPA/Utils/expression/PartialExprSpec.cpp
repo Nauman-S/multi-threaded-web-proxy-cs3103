1be5e3029ee306a10365857304341de53425f20f
awp <49505301+wangpeialex@users.noreply.github.com>
2022-09-29 21:51:40 +0800

    Implement new subexpression matching

--- Team34/Code34/SPA/Utils/expression/PartialExprSpec.cpp
+++ Team34/Code34/SPA/Utils/expression/PartialExprSpec.cpp
@@ -4,19 +4,18 @@ bool PartialExprSpec::IsMatch(Expr expr)
 {
 	string full_expr = expr.GetPostfix();
 	int fullstr_len = full_expr.size();
-	int substr_len = post_expr_.length();
+	int substr_len = post_expr_.size();
 	auto is_sub_expr{ false };
 	int pos = full_expr.find(post_expr_, 0);
 
-	while (pos < fullstr_len) {
+	while (pos != string::npos && pos < fullstr_len) {
 		
-		if ((pos == 0 || full_expr[pos - 1] == ' ') && (pos + substr_len == fullstr_len || full_expr[pos + substr_len] == ' ')) {
+		if ((pos == 0 || full_expr.at(pos - 1) == ' ') && (pos + substr_len == fullstr_len || full_expr.at(pos + substr_len) == ' ')) {
 			is_sub_expr = true;
 			break;
 		}
-		pos = full_expr.find(post_expr_, pos + 1);
+		pos = full_expr.find(post_expr_, pos + substr_len);
 	}
 
 	return is_sub_expr;
-
 }
