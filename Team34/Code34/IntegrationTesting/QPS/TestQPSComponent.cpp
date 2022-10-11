b25294b58a964ee49e8ad5a1e1297c43802c4566
Nauman Sajid <e0389102@u.nus.edu>
2022-10-11 14:25:06 +0800

    Add Integration Testing for QPS

--- Team34/Code34/IntegrationTesting/QPS/TestQPSComponent.cpp
+++ Team34/Code34/IntegrationTesting/QPS/TestQPSComponent.cpp
@@ -21,7 +21,7 @@ namespace IntegrationTesting
 		std::shared_ptr<ReadPKBManager> pkb_stub = std::make_shared<ReadPKBManagerStub>();
 		shared_ptr<DataRetriever> data_retriever_ = shared_ptr<DataRetriever>(new DataRetriever(pkb_stub));
 	public:
-		TEST_METHOD(BasicQPSComponentTest)
+		TEST_METHOD(VariableQPSComponentTest)
 		{
 			string query_string = "variable v; Select v";
 
@@ -47,6 +47,239 @@ namespace IntegrationTesting
 			vector<string> correct_res = { "x","y","z" };
 
 			for (string key: correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(BooleanTrueQPSComponentTest)
+		{
+			string query_string = "stmt s1; Select BOOLEAN such that Follows(_,s1)";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 1);
+
+
+			vector<string> correct_res = { "TRUE" };
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(BooleanFalseQPSComponentTest)
+		{
+			string query_string = "stmt s1; Select BOOLEAN such that Follows(6,s1)";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 1);
+
+
+			vector<string> correct_res = { "FALSE" };
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(BooleanTrueQPSComponentWith1)
+		{
+			string query_string = "Select BOOLEAN with 12 = 12";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 1);
+
+			vector<string> correct_res = { "TRUE" };
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(BooleanTrueQPSComponentWith2)
+		{
+			string query_string = "procedure p,p2;\n Select BOOLEAN with p.procName  = \"Foo1\" and p2.procName = \"Nine\"";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 1);
+
+			vector<string> correct_res = { "TRUE" };
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(BooleanTrueQPSComponentCalls)
+		{
+			string query_string = "Select BOOLEAN such that Calls (_,_)";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 1);
+
+			vector<string> correct_res = { "TRUE" };
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(MultipleReturnQPSComponentModifies)
+		{
+			string query_string = "procedure p, q; assign a; Select <p, q> such that Modifies(\"func1\", \"x\")";
+
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+			Assert::IsTrue(res.size() == 9);
+
+			vector<string> correct_res = { "Foo1 Foo1", "Foo1 bar", "Foo1 Nine", "bar bar", "bar Foo1", "bar Nine" ,"Nine Nine", "Nine bar", "Nine Foo1"};
+
+			for (string key : correct_res) {
+				if (!std::count(res.begin(), res.end(), key)) {
+					isValid = false;
+				}
+			}
+
+			Assert::IsTrue(isValid);
+		}
+
+		TEST_METHOD(MultipleReturnQPSComponentSuchThatCalls)
+		{
+			//string query_string = "assign a; while w; Select <a.stmt#, w> such that Calls*(\"Foo1\", \"Nine\") and Modifies(\"func1\", \"x\")";
+			string query_string = "assign a; while w; Select <a, w> such that Calls*(\"Foo1\", \"Nine\") and Modifies(\"func1\", \"x\")";
+
+			bool isValid = true;
+			shared_ptr<Query> query;
+
+			try {
+				query = query_builder_->GetQuery(query_string);
+			}
+			catch (...) {
+				isValid = false;
+			}
+
+			Assert::IsTrue(isValid);
+
+			QueryEvaluator evaluator(*query, *data_retriever_);
+			evaluator.Evaluate();
+			vector<std::string> res = evaluator.ExtractResult();
+
+			Assert::IsTrue(res.size() == 4);
+
+			vector<string> correct_res = { "2 1", "4 1", "5 1", "6 1" };
+			for (string key : correct_res) {
 				if (!std::count(res.begin(), res.end(), key)) {
 					isValid = false;
 				}
