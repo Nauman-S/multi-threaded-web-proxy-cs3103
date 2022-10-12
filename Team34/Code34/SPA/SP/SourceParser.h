f708c8c89e5ca0af46e2402b56ed287f2900de65
Gao Gui <e0376992@u.nus.edu>
2022-10-12 19:27:50 +0800

    remove param in parser function

--- Team34/Code34/SPA/SP/SourceParser.h
+++ Team34/Code34/SPA/SP/SourceParser.h
@@ -29,15 +29,19 @@ public:
 	std::shared_ptr<ProgramNode> Parse(std::shared_ptr<std::vector<SourceToken>>);
 
 private:
-	std::shared_ptr<ProcedureASTNode> ParseProcedure(std::shared_ptr<std::vector<SourceToken>>, int&, int&);
-	std::shared_ptr<StatementASTNode> ParseStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<CallStatementASTNode> ParseCallStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<ReadStatementASTNode> ParseReadStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<PrintStatementASTNode> ParsePrintStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<IfStatementASTNode> ParseIfStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<WhileStatementASTNode> ParseWhileStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<AssignStatementASTNode> ParseAssignStatement(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
-	std::shared_ptr<ConditionExpression> ParseConditionExpression(std::shared_ptr<std::vector<SourceToken>>, int&, int&, Procedure&);
+    int token_idx = 0;
+    int line_idx = 0;
+    void IncrementTokenIdx();
+    void IncrementLineIdex();
+	std::shared_ptr<ProcedureASTNode> ParseProcedure(std::shared_ptr<std::vector<SourceToken>>);
+	std::shared_ptr<StatementASTNode> ParseStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<CallStatementASTNode> ParseCallStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<ReadStatementASTNode> ParseReadStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<PrintStatementASTNode> ParsePrintStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<IfStatementASTNode> ParseIfStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<WhileStatementASTNode> ParseWhileStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<AssignStatementASTNode> ParseAssignStatement(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
+	std::shared_ptr<ConditionExpression> ParseConditionExpression(std::shared_ptr<std::vector<SourceToken>>, Procedure&);
 
 	std::string JoinWithDelimiter(std::vector<std::string>&, std::string);
 };
