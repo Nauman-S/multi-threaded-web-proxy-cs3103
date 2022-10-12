a9e929d0b691cd87fb8c4d295a4ec0e7150772d9
awp <49505301+wangpeialex@users.noreply.github.com>
2022-10-12 15:41:00 +0800

    Refactor reference classes to support attr type

--- Team34/Code34/SPA/QPS/reference/StmtRef.h
+++ Team34/Code34/SPA/QPS/reference/StmtRef.h
@@ -5,12 +5,16 @@
 #include "IntValueableRef.h"
 #include "../../Utils/type/RefType.h"
 
-class StmtRef :
-    public IntValueableRef
-{
+class StmtRef : public IntValueableRef {
+public:
     using IntValueableRef::IntValueableRef;
 
-public:
+    StmtRef(ValType val_type, std::string val)
+        : IntValueableRef{ val_type, val, AttrType::kStmtNum } {
+    };
+
     const RefType GetRefType() override { return RefType::kStmtRef; }
+
+    bool IsAttrTypeDefault() override { return true; }
 };
 
