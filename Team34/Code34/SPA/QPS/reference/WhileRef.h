178d852dbb3240f6875db5c43b916e5ce2626a44
awp <49505301+wangpeialex@users.noreply.github.com>
2022-10-12 23:44:08 +0800

    Refactor stmt ref to reduce redundancy

--- Team34/Code34/SPA/QPS/reference/WhileRef.h
+++ Team34/Code34/SPA/QPS/reference/WhileRef.h
@@ -4,18 +4,9 @@
 #include "../../Utils/type/RefType.h"
 
 class WhileRef : public StmtRef {
-private:
-    const AttrType default_attr_type_{ AttrType::kStmtNum };
-
 public:
     using StmtRef::StmtRef;
 
-    WhileRef(ValType val_type, std::string val)
-        : StmtRef{ val_type, val, default_attr_type_ } {
-    };
-
     const RefType GetRefType() override { return RefType::kWhileRef; }
-
-    bool IsAttrTypeDefault() override { return attr_type_ == default_attr_type_; }
 };
 
