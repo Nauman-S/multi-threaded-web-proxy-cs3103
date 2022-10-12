1cbe1ececb8c7d0b7a0f09fe2a81409ae8a3796e
tanzhuoyao-nus <tanzhuoyao@gmail.com>
2022-10-12 12:22:49 +0800

    Implement StatementManager methods

--- Team34/Code34/SPA/PKB/manager/StatementManager.h
+++ Team34/Code34/SPA/PKB/manager/StatementManager.h
@@ -22,10 +22,16 @@ public:
 	std::shared_ptr<std::unordered_set<StmtNum>> GetCallsStatementFromProcedure(Procedure proc);
 	std::shared_ptr<std::unordered_set<StmtNum>> GetPrintStatementFromVariable(Variable var);
 	std::shared_ptr<std::unordered_set<StmtNum>> GetReadStatementFromVariable(Variable var);
+	std::shared_ptr<Procedure> GetCallsProcedureFromStatement(StmtNum stmt_num);
+	std::shared_ptr<Variable> GetPrintVariableFromStatement(StmtNum stmt_num);
+	std::shared_ptr<Variable> GetReadVariableFromStatement(StmtNum stmt_num);
 private:
 	ManyToOneRelationStore<StmtNum, RefType> statement_store_;
 	std::unordered_set<StmtNum> statements_;
 	std::unordered_map<Procedure, std::unordered_set<StmtNum>> calls_statements_;
+	std::unordered_map<StmtNum, Procedure> calls_procedures_;
 	std::unordered_map<Variable, std::unordered_set<StmtNum>> print_statements_;
+	std::unordered_map<StmtNum, Variable> print_variables_;
 	std::unordered_map<Variable, std::unordered_set<StmtNum>> read_statements_;
+	std::unordered_map<StmtNum, Variable> read_variables_;
 };
