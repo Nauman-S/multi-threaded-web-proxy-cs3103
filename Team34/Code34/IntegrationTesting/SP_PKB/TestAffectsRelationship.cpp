c789d0df1fa904b0b8cef8cac9f33b3499839cdc
Lye Jia Yang <lyejy99@gmail.com>
2022-10-13 17:59:53 +0800

    Add integration tests for get all cause and effect statements

--- Team34/Code34/IntegrationTesting/SP_PKB/TestAffectsRelationship.cpp
+++ Team34/Code34/IntegrationTesting/SP_PKB/TestAffectsRelationship.cpp
@@ -44,6 +44,20 @@ namespace IntegrationTesting
 			Assert::IsFalse(read->CheckAffects(4, 3));
 		}
 
+		TEST_METHOD(TestCheckAffectsForReadCalls) 
+		{
+			// Read modification
+			Assert::IsTrue(read->CheckAffects(18, 22));
+			Assert::IsTrue(read->CheckAffects(19, 22));
+			Assert::IsTrue(read->CheckAffects(19, 23));
+			Assert::IsFalse(read->CheckAffects(20, 22));
+			Assert::IsFalse(read->CheckAffects(20, 23));
+
+			// Calls modification
+			Assert::IsTrue(read->CheckAffects(25, 27));
+			Assert::IsFalse(read->CheckAffects(24, 27));
+		}
+
 		TEST_METHOD(TestCheckAffectsOnIfStatement)
 		{
 			Assert::IsTrue(read->CheckAffects(7, 11));
@@ -121,10 +135,77 @@ namespace IntegrationTesting
 
 		TEST_METHOD(TestGetCauseStmtsFromStmtOnWhileLoop)
 		{
-			// Result: 13, 15, 16
-			Assert::AreEqual(3, int(read->GetCauseStmtsFromStmt(15)->size()));
-			// Result: 13, 16
-			Assert::AreEqual(2, int(read->GetCauseStmtsFromStmt(16)->size()));
+			shared_ptr<unordered_set<StmtNum>> actual_stmt_15 = read->GetCauseStmtsFromStmt(15);
+			unordered_set<StmtNum> expected = {13, 15, 16};
+			Assert::IsTrue(*actual_stmt_15 == expected);
+
+			shared_ptr<unordered_set<StmtNum>> actual_stmt_16 = read->GetCauseStmtsFromStmt(16);
+			expected = {13, 16};
+			Assert::IsTrue(*actual_stmt_16 == expected);
+		}
+
+		TEST_METHOD(TestGetAllEffectStmtsNoNesting)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllEffectStmts();
+			Assert::IsTrue(actual->find(3) != actual->end());
+			Assert::IsTrue(actual->find(5) != actual->end());
+			Assert::IsTrue(actual->find(6) != actual->end());
+			Assert::IsFalse(actual->find(4) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllEffectStmtsSingleIf)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllEffectStmts();
+			Assert::IsTrue(actual->find(11) != actual->end());
+			Assert::IsTrue(actual->find(12) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllEffectStmtsSingleWhile)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllEffectStmts();
+			Assert::IsTrue(actual->find(15) != actual->end());
+			Assert::IsTrue(actual->find(16) != actual->end());
+			Assert::IsTrue(actual->find(17) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllEffectStmtsIfWhile)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllEffectStmts();
+			Assert::IsTrue(actual->find(32) != actual->end());
+			Assert::IsTrue(actual->find(33) != actual->end());
+			Assert::IsTrue(actual->find(33) != actual->end());
+			Assert::IsTrue(actual->find(37) != actual->end());
+			Assert::IsFalse(actual->find(34) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllCauseStmtsNoNesting)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllCauseStmts();
+			Assert::IsTrue(actual->find(1) != actual->end());
+			Assert::IsTrue(actual->find(2) != actual->end());
+			Assert::IsTrue(actual->find(4) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllCauseStmtsSingleIf)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllCauseStmts();
+			Assert::IsTrue(actual->find(7) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllCauseStmtsSingleWhile)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllCauseStmts();
+			Assert::IsTrue(actual->find(13) != actual->end());
+			Assert::IsTrue(actual->find(15) != actual->end());
+			Assert::IsTrue(actual->find(16) != actual->end());
+		}
+
+		TEST_METHOD(TestGetAllCauseStmtsIfWhile)
+		{
+			shared_ptr<unordered_set<StmtNum>> actual = read->GetAllCauseStmts();
+			Assert::IsTrue(actual->find(29) != actual->end());
+			Assert::IsTrue(actual->find(30) != actual->end());
+			Assert::IsTrue(actual->find(38) != actual->end());
 		}
 	};
 }
