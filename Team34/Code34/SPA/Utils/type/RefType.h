42f35904496848949851f9fdabba7f1e3b3f41d7
Wang Pei <E0376916@u.nus.edu>
2022-10-19 18:02:23 +0800

    Add explanation for enum classes

--- Team34/Code34/SPA/Utils/type/RefType.h
+++ Team34/Code34/SPA/Utils/type/RefType.h
@@ -1,16 +1,23 @@
 #pragma once
-enum class RefType
-{
-	kProcRef,
-	kVarRef,
-	kConstRef,
-	kStmtRef,
-	kAssignRef,
-	kCallRef,
-	kIfRef,
-	kWhileRef,
-	kReadRef,
-	kPrintRef,
-	kUnknown
+
+/*
+* This enum class works as a type contract between ReadPKBManager and DataRetriever.
+* To allow ReadPKBManager to get or filter return results as indicated by DataRetriever.
+*
+* Design decision:
+*
+*/
+enum class RefType {
+    kProcRef,
+    kVarRef,
+    kConstRef,
+    kStmtRef,
+    kAssignRef,
+    kCallRef,
+    kIfRef,
+    kWhileRef,
+    kReadRef,
+    kPrintRef,
+    kUnknown
 };
 
