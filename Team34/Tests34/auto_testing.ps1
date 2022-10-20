78d4b9ab5675a3ba2abd39c8a618a2bef690cf92
Nauman Sajid <e0389102@u.nus.edu>
2022-10-20 19:23:31 +0800

    update github workflow

--- Team34/Tests34/auto_testing.ps1
+++ Team34/Tests34/auto_testing.ps1
@@ -7,12 +7,12 @@ $autotester_file = "..\Code34\Debug\AutoTester.exe"
 $condition = Test-Path -Path $autotester_file -PathType Leaf
 $port = 2333
 
-# if ($condition) {
-#     Write-Output "AutoTester binary is found"
-# } else {
-#     Write-Output "AutoTester binary is not found"
-#     Exit
-# }
+if ($condition) {
+    Write-Output "AutoTester binary is found"
+} else {
+    Write-Output "AutoTester binary is not found"
+    Exit
+}
 
 
 
