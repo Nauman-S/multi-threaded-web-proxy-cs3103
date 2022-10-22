da0b63b21007799d0c7c3c78eed661d0bbe6d6ec
tanzhuoyao-nus <tanzhuoyao@gmail.com>
2022-10-22 13:00:52 +0800

    Add GetAllCauses and Effects stmts integration tests

--- Team34/Code34/IntegrationTesting/TestAffectsTRelationship.cpp
+++ Team34/Code34/IntegrationTesting/TestAffectsTRelationship.cpp
@@ -56,5 +56,50 @@ namespace IntegrationTesting
             Assert::IsTrue(all_effects->find(11) != all_effects->end());
             Assert::IsTrue(all_effects->find(12) != all_effects->end());
         }
+
+        TEST_METHOD(TestGetAllEffectStmtsFromStmtSingleWhile)
+        {
+            std::shared_ptr<std::unordered_set<StmtNum>> all_effects = read->GetAllEffectStmtsFromStmt(13, RefType::kStmtRef);
+            Assert::AreEqual(4, int(all_effects->size()));
+            Assert::IsTrue(all_effects->find(15) != all_effects->end());
+            Assert::IsTrue(all_effects->find(16) != all_effects->end());
+            Assert::IsTrue(all_effects->find(17) != all_effects->end());
+            Assert::IsTrue(all_effects->find(18) != all_effects->end());
+        }
+
+        TEST_METHOD(TestGetAllCauseStmtsFromStmtNoNesting)
+        {
+            std::shared_ptr<std::unordered_set<StmtNum>> all_causes = read->GetAllCauseStmtsFromStmt(6, RefType::kStmtRef);
+            Assert::AreEqual(3, int(all_causes->size()));
+            Assert::IsTrue(all_causes->find(4) != all_causes->end());
+            Assert::IsTrue(all_causes->find(3) != all_causes->end());
+            Assert::IsTrue(all_causes->find(1) != all_causes->end());
+        }
+
+        TEST_METHOD(TestGetAllCauseStmtsFromStmtSingleIf)
+        {
+            std::shared_ptr<std::unordered_set<StmtNum>> all_causes = read->GetAllCauseStmtsFromStmt(12, RefType::kStmtRef);
+            Assert::AreEqual(3, int(all_causes->size()));
+            Assert::IsTrue(all_causes->find(10) != all_causes->end());
+            Assert::IsTrue(all_causes->find(9) != all_causes->end());
+            Assert::IsTrue(all_causes->find(7) != all_causes->end());
+
+            all_causes = read->GetAllCauseStmtsFromStmt(11, RefType::kStmtRef);
+            Assert::AreEqual(1, int(all_causes->size()));
+            Assert::IsTrue(all_causes->find(7) != all_causes->end());
+        }
+
+        TEST_METHOD(TestGetAllCauseStmtsFromStmtSingleWhile)
+        {
+            std::shared_ptr<std::unordered_set<StmtNum>> all_causes = read->GetAllCauseStmtsFromStmt(18, RefType::kStmtRef);
+            Assert::AreEqual(1, int(all_causes->size()));
+            Assert::IsTrue(all_causes->find(13) != all_causes->end());
+
+            all_causes = read->GetAllCauseStmtsFromStmt(17, RefType::kStmtRef);
+            Assert::AreEqual(3, int(all_causes->size()));
+            Assert::IsTrue(all_causes->find(16) != all_causes->end());
+            Assert::IsTrue(all_causes->find(15) != all_causes->end());
+            Assert::IsTrue(all_causes->find(13) != all_causes->end());
+        }
     };
 }
