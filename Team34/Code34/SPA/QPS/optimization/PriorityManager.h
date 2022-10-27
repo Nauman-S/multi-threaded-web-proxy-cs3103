a6276a61324f707746e8c2b1e0731eb938bfcd09
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2022-10-27 10:49:24 +0800

    Adjust the priority

--- Team34/Code34/SPA/QPS/optimization/PriorityManager.h
+++ Team34/Code34/SPA/QPS/optimization/PriorityManager.h
@@ -52,53 +52,55 @@ private:
     // The priority is determined by the estimated size of the result and time taken to get the result
     std::unordered_map<std::pair<ClauseType, int>, Priority, pair_hash> priority_map_ = {
         // SMALLER priority number has HIGHER priority
-        { std::make_pair(ClauseType::kParentRel, 1), 0},
+        {std::make_pair(ClauseType::kParentRel, 1), 0},
         {std::make_pair(ClauseType::kFollowsRel, 1), 0},
         {std::make_pair(ClauseType::kCallsRel, 1), 0},
         {std::make_pair(ClauseType::kNextRel, 1), 0},
 
-        {std::make_pair(ClauseType::kModifiesPRel, 1), 1},
-        {std::make_pair(ClauseType::kUsesPRel, 1), 1},
+        {std::make_pair(ClauseType::kModifiesPRel, 1), 2},
+        {std::make_pair(ClauseType::kUsesPRel, 1), 2},
 
-        {std::make_pair(ClauseType::kModifiesSRel, 1), 2},
-        {std::make_pair(ClauseType::kUsesSRel, 1), 2},
-
-        {std::make_pair(ClauseType::kAssignPattern, 1), 3},
+        {std::make_pair(ClauseType::kModifiesSRel, 1), 3},
+        {std::make_pair(ClauseType::kUsesSRel, 1), 3},
         {std::make_pair(ClauseType::kIfPattern, 1), 3},
         {std::make_pair(ClauseType::kWhilePattern, 1), 3},
         {std::make_pair(ClauseType::kAffectsRel, 1), 3},
 
+        {std::make_pair(ClauseType::kAssignPattern, 1), 4},
         {std::make_pair(ClauseType::kWith, 1), 4},
+        {std::make_pair(ClauseType::kParentTRel, 1), 4},
 
-        {std::make_pair(ClauseType::kParentTRel, 1), 5},
         {std::make_pair(ClauseType::kFollowsTRel, 1), 5},
         {std::make_pair(ClauseType::kCallsTRel, 1), 5},
-        {std::make_pair(ClauseType::kNextTRel, 1), 5},
-        {std::make_pair(ClauseType::kAffectsTRel, 1), 5},
+        
+        {std::make_pair(ClauseType::kAffectsTRel, 1), 7},
+        {std::make_pair(ClauseType::kCallsRel, 2), 7},
 
-        {std::make_pair(ClauseType::kParentRel, 2), 0},
-        {std::make_pair(ClauseType::kFollowsRel, 2), 0},
-        {std::make_pair(ClauseType::kCallsRel, 2), 0},
-        {std::make_pair(ClauseType::kNextRel, 2), 0},
+        {std::make_pair(ClauseType::kNextTRel, 1), 8},
+        {std::make_pair(ClauseType::kParentRel, 2), 8},
 
-        {std::make_pair(ClauseType::kModifiesPRel, 2), 1},
-        {std::make_pair(ClauseType::kUsesPRel, 2), 1},
+        {std::make_pair(ClauseType::kModifiesPRel, 2), 10},
+        {std::make_pair(ClauseType::kModifiesSRel, 2), 10},
+        {std::make_pair(ClauseType::kParentTRel, 2), 10},
+        {std::make_pair(ClauseType::kCallsTRel, 2), 10},
 
-        {std::make_pair(ClauseType::kModifiesSRel, 2), 2},
-        {std::make_pair(ClauseType::kUsesSRel, 2), 2},
+        {std::make_pair(ClauseType::kUsesPRel, 2), 15},
+        { std::make_pair(ClauseType::kUsesSRel, 2), 15 },
 
-        {std::make_pair(ClauseType::kAssignPattern, 2), 3},
-        {std::make_pair(ClauseType::kIfPattern, 2), 3},
-        {std::make_pair(ClauseType::kWhilePattern, 2), 3},
-        {std::make_pair(ClauseType::kAffectsRel, 2), 3},
+        {std::make_pair(ClauseType::kFollowsRel, 2), 20},
+        {std::make_pair(ClauseType::kNextRel, 2), 20},
+        {std::make_pair(ClauseType::kIfPattern, 2), 20},
+        {std::make_pair(ClauseType::kWhilePattern, 2), 20},
+        
+        {std::make_pair(ClauseType::kWith, 2), 30},
+        {std::make_pair(ClauseType::kAssignPattern, 2), 30},
+        
+        {std::make_pair(ClauseType::kFollowsTRel, 2), 40},
 
-        {std::make_pair(ClauseType::kWith, 2), 4},
-
-        {std::make_pair(ClauseType::kParentTRel, 2), 5},
-        {std::make_pair(ClauseType::kFollowsTRel, 2), 5},
-        {std::make_pair(ClauseType::kCallsTRel, 2), 5},
-        {std::make_pair(ClauseType::kNextTRel, 2), 5},
-        {std::make_pair(ClauseType::kAffectsTRel, 2), 5}
+        {std::make_pair(ClauseType::kAffectsRel, 2), 50},        
+        {std::make_pair(ClauseType::kNextTRel, 2), 50},
+        
+        {std::make_pair(ClauseType::kAffectsTRel, 2), 100}
     };
 
 public:
