3c6b7fa1236c4b27fee1b70f01ff0310c5e6ab57
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2022-10-31 14:53:24 +0800

    Make SyntaxError and SemanticErrror inherit from Error

--- Team34/Code34/SPA/QPS/parser/SemanticError.cpp
+++
@@ -1,9 +0,0 @@
-#include "SemanticError.h"
-
-SemanticError::SemanticError(std::string message) {
-	this->message = message;
-}
-
-std::string SemanticError::what() const throw() {
-	return "Invalid Query Semantic: " + this->message;
-}
