78afedf192de53310d457c536086299e98838ac8
Harry-Gao-H <59434153+Harry-Gao-H@users.noreply.github.com>
2022-11-01 11:10:46 +0800

    Remove commented code

--- Team34/Code34/SPA/QPS/optimization/PriorityManager.h
+++ Team34/Code34/SPA/QPS/optimization/PriorityManager.h
@@ -9,35 +9,6 @@
 
 class PriorityManager {
 private:
-    //std::unordered_map<ClauseType, Priority> priority_map_ = {
-    //    // SMALLER priority number has HIGHER priority (should just sort ascendingly)
-    //    {ClauseType::kParentRel, 0},
-    //    {ClauseType::kFollowsRel, 0},
-    //    {ClauseType::kCallsRel, 0},
-    //    {ClauseType::kNextRel, 0},
-    //    
-    //    {ClauseType::kModifiesPRel, 1},
-    //    {ClauseType::kUsesPRel, 1},
-
-    //    {ClauseType::kModifiesSRel, 2},
-    //    {ClauseType::kUsesSRel, 2},
-
-    //    {ClauseType::kAssignPattern, 3},
-    //    {ClauseType::kIfPattern, 3},
-    //    {ClauseType::kWhilePattern, 3},
-    //    {ClauseType::kAffectsRel, 3},
-
-    //    {ClauseType::kWith, 4},
-
-    //    {ClauseType::kParentTRel, 5},
-    //    {ClauseType::kFollowsTRel, 5},
-    //    {ClauseType::kCallsTRel, 5},
-    //    {ClauseType::kNextTRel, 5},
-    //    {ClauseType::kAffectsTRel, 5},
-    //};
-
-
-
     // Refers to https://stackoverflow.com/questions/32685540/why-cant-i-compile-an-unordered-map-with-a-pair-as-key
     struct pair_hash {
         template <class T1, class T2>
